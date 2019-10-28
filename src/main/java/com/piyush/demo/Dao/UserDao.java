01e1b2ed291b56468a07f87adf23ef8bb4aaffbb
PIYUSH KUMAR <pikumar@quotient.com>
2019-10-28 18:33:05 +0530

    Added UserDao

---
+++ src/main/java/com/piyush/demo/Dao/UserDao.java
@@ -0,0 +1,37 @@
+package com.piyush.demo.Dao;
+
+
+import com.piyush.demo.model.User;
+import com.piyush.demo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserDao {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    // To Save an Single User
+    public User save(User user){
+        return userRepository.save(user);
+    }
+
+    //To Save List / Multiple Users
+    public List<User> saveAll(List<User> users){
+        return userRepository.saveAll(users);
+    }
+
+    //To Search The User by their userId
+    public User findOne(Long userId){
+        return userRepository.findByUserId(userId);
+    }
+
+    //To Get all the Users
+    public List<User> getAll(){
+        return userRepository.findAll();
+    }
+
+}
--- src/main/java/com/piyush/demo/repository/UserRepository.java
+++ src/main/java/com/piyush/demo/repository/UserRepository.java
@@ -2,8 +2,12 @@ package com.piyush.demo.repository;
 
 import com.piyush.demo.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    @Query(value = "SELECT * FROM UserProfile WHERE UserId =: userId", nativeQuery = true)
+    User findByUserId(Long userId);
 }
