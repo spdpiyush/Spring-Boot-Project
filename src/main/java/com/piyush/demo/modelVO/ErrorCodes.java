e260a3bdcc8d7c110b56745843e2c11226292ae3
PIYUSH KUMAR <pikumar@quotient.com>
2019-10-29 11:22:02 +0530

    Added get user by UserId controller & modification in the Error Codes

--- src/main/java/com/piyush/demo/modelVO/ErrorCodes.java
+++ src/main/java/com/piyush/demo/modelVO/ErrorCodes.java
@@ -9,7 +9,11 @@ public enum ErrorCodes {
 
         USER_ALREADY_EXIST(1000,"User Already Exist"),
         USER_NOT_CREATED(1001,"Error in Creating the User"),
-        USERS_NOT_CREATED(1002,"Error in Creating List of Users");
+        USERS_NOT_CREATED(1002,"Error in Creating List of Users"),
+        USER_NOT_PRESENT(1003,"No User with that UserId"),
+        ERROR_IN_GET_USER(1004,"Error Occured to get an User"),
+        ERROR_IN_PUT_USER(1005,"Error Occured to put a User"),
+        ERROR_IN_PUT_USERS(1006,"Error Occured to put List of Users");
 
         private final Integer value;
         private final String phrase;
