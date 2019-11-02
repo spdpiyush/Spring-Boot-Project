579737a544160f4300c7ef123262af9f322ded15
PIYUSH KUMAR <pikumar@quotient.com>
2019-11-02 13:08:24 +0530

    CreatePost RestController Created and added some error codes

--- src/main/java/com/piyush/demo/controller/UserController.java
+++ src/main/java/com/piyush/demo/controller/UserController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/piyush")
+@RequestMapping("/api")
 public class UserController {
 
     @Autowired
