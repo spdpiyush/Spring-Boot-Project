f8d43cf645d16b0e756344efe39965eb3cda8874
PIYUSH KUMAR <piyushchamp.pkp@gmail.com>
2019-12-29 20:57:07 +0530

    adding KafkaConfiguration

---
+++ src/main/java/com/piyush/demo/config/KafkaConfiguration.java
@@ -0,0 +1,50 @@
+package com.piyush.demo.config;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+import java.util.Properties;
+
+/**
+ * Created By : Piyush Kumar
+ * on 2019-12-29 & 20:47
+ */
+
+@Component
+public class KafkaConfiguration {
+
+    @Bean
+    public Properties kafkaProducerConfig(){
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"127.0.0.1:9092");
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        return properties;
+    }
+
+    @Bean
+    public Properties kafkaConsumerConfig(){
+        Properties properties = new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,"127.0.0.1:9092");
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+
+        return properties;
+    }
+
+    @Bean
+    public KafkaProducer<String, String> createKafkaProducer(){
+        return new KafkaProducer<String, String>(kafkaProducerConfig());
+    }
+
+    public KafkaConsumer<String, String> createKafkaConsumer(){
+        return new KafkaConsumer<String, String>(kafkaConsumerConfig());
+    }
+}
